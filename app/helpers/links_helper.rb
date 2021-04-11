c67582cbab413bad326862236dfd52cb8c3dc1dd
Vinayak Powar <powarvinayak@gmail.com>
2021-04-11 23:32:49 +0530

    helper function to get top 3 countries for each link and displayed on analytics page.

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,12 @@
 module LinksHelper
+
+    def get_top_countries (link)
+        top_countries = []
+        link.link_country_counts.each do |row|
+            top_countries.push(row.country) if row.country
+            break if top_countries.length == 3
+        end
+        top_countries.join(", ")
+    end
+
 end
