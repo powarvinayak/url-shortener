1995880926f751c21d046301de391d639b7b8f31
Vinayak Powar <powarvinayak@gmail.com>
2021-04-11 19:43:50 +0530

    saving link in database if valid and response in js format.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,23 @@
 class LinksController < ApplicationController
   def index
+    @link = Link.new
   end
 
   def create
+    @link = Link.new(link_params)
+    respond_to do |format|
+      if @link.save
+        format.js
+      else
+        format.js {render :index}
+      end
+    end
   end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:long_url)
+  end
+
 end
