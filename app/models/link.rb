a8f2df03de0c6c42f1b94c54b14f3f44f08e1b27
Vinayak Powar <powarvinayak@gmail.com>
2021-04-11 19:39:58 +0530

    generation of short_url and validation for uniqueness.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,12 @@
+require 'securerandom'
 class Link < ApplicationRecord
     validates_presence_of :long_url
     validates :long_url, format: URI::regexp(%w[http https])
     validates_uniqueness_of :short_url
+    before_validation :generate_short_url
+
+    def generate_short_url
+        self.short_url = SecureRandom.uuid[0..4] if self.short_url.nil?
+    end   
 
 end
